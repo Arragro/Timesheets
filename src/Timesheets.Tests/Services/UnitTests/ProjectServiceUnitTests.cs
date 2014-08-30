d742fcad218920fd282c844b7a4cd8b39d98d80b
Michael Jewell <michael_jewell@hotmail.co.uk>
2014-08-30 14:36:15 +1200

    Implemented the Validation of Model Property Attributes for a Project.

--- src/Timesheets.Tests/Services/UnitTests/ProjectServiceUnitTests.cs
+++ src/Timesheets.Tests/Services/UnitTests/ProjectServiceUnitTests.cs
@@ -27,6 +27,29 @@ namespace Timesheets.Tests.Services.UnitTests
     public class ProjectServiceUnitTests
     {
         [Fact]
+        public void ModelAttributesValidateCorrectly()
+        {
+            var unityContainer = InMemoryUnityContainer.GetInMemoryContainer();
+            var projectService = unityContainer.Resolve<ProjectService>();
+
+            var errors = projectService.ValidateModel(new Project());
+            Assert.Equal(1, errors.Count);
+
+            errors = projectService.ValidateModel(
+                        new Project 
+                        { 
+                            Name = new String('X', 51),
+                            Code = new String('X', 21),
+                            PurchaseOrderNumber = new String('X', 21)
+                        });
+            Assert.Equal(3, errors.Count);
+            Assert.NotNull(errors.SingleOrDefault(x => x.MemberNames.Contains("Name")));
+            Assert.NotNull(errors.SingleOrDefault(x => x.MemberNames.Contains("Code")));
+            Assert.NotNull(errors.SingleOrDefault(x => x.MemberNames.Contains("PurchaseOrderNumber")));
+        }
+                
+
+        [Fact]
         public void NoUserIdForProject()
         {
             var userId = Guid.NewGuid();
