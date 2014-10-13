77cb5050a87aaaafa147c2b508f27eff2a7fdf8f
Michael Jewell <michael_jewell@hotmail.co.uk>
2014-10-13 22:13:41 +1300

    Added Project Invitation Service and modified the model accordingly.
    Related Work Items: #140

---
+++ src/Timesheets.BusinessLayer/Services/ProjectInvitationService.cs
@@ -0,0 +1,67 @@
+﻿using Arragro.Common.ServiceBase;
+using System;
+using System.Linq;
+using Timesheets.DataLayer.Interfaces;
+using Timesheets.DataLayer.Models;
+
+namespace Timesheets.BusinessLayer.Services
+{
+    public class ProjectInvitationService : AuditableService<IProjectInvitationRepository, ProjectInvitation, Guid, Guid>
+    {
+        public const string PROJECID_NOT_SET = "ProjectId is not set";
+        public const string USER_IS_NOT_NULL_NOT_SET = "UserId is not null and is not set";
+        public const string INVITATIONCODE_IS_NOT_SET = "Invitation Code is not set";
+        public const string INVITATION_ACCEPTED_BEFORE_SENT = "Invitation has been accepted before it was sent";
+
+        public ProjectInvitationService(
+            IProjectInvitationRepository projectInvitationRepository)
+            : base(projectInvitationRepository)
+        {
+        }
+
+        private void EnsureProjectIdIsSet(ProjectInvitation projectInvitation)
+        {
+            if (projectInvitation.ProjectId == null ||
+                projectInvitation.ProjectId == default(Guid))
+                RulesException.ErrorFor(x => x.ProjectId, PROJECID_NOT_SET);
+        }
+
+        private void EnsureUserIdIsSet(ProjectInvitation projectInvitation)
+        {
+            if (projectInvitation.UserId.HasValue &&
+                projectInvitation.UserId.Value == default(Guid))
+                RulesException.ErrorFor(x => x.UserId, USER_IS_NOT_NULL_NOT_SET);
+        }
+
+        private void EnsureInvitationCodeIsSet(ProjectInvitation projectInvitation)
+        {
+            if (projectInvitation.InvitationCode == null ||
+                projectInvitation.InvitationCode == default(Guid))
+                RulesException.ErrorFor(x => x.InvitationCode, INVITATIONCODE_IS_NOT_SET);
+        }
+
+        private void EnsureInvitationAcceptedIsValid(ProjectInvitation projectInvitation)
+        {
+            if (projectInvitation.InvitationAccepted.HasValue &&
+                !projectInvitation.InvitationSent)
+                RulesException.ErrorFor(x => x.InvitationAccepted, INVITATION_ACCEPTED_BEFORE_SENT);
+        }
+
+        protected override void ValidateModelRules(ProjectInvitation model)
+        {
+            EnsureProjectIdIsSet(model);
+            EnsureUserIdIsSet(model);
+            EnsureInvitationCodeIsSet(model);
+            EnsureInvitationAcceptedIsValid(model);
+
+            if (RulesException.Errors.Any()) throw RulesException;
+        }
+
+        public override ProjectInvitation InsertOrUpdate(ProjectInvitation model, Guid userId)
+        {
+            var add = default(Guid) == model.ProjectInvitationId;
+            AddOrUpdateAudit(model, userId, add);
+            return Repository.InsertOrUpdate(model, add);
+        }
+    }
+}
--- src/Timesheets.DataLayer/Models/ProjectInvitation.cs
+++ src/Timesheets.DataLayer/Models/ProjectInvitation.cs
@@ -1,13 +1,18 @@
 ﻿using Arragro.Common.BusinessRules;
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Timesheets.DataLayer.Models
 {
     public class ProjectInvitation : Auditable<Guid>
     {
-        public Guid InvitationId { get; set; }
+        public Guid ProjectInvitationId { get; set; }
         public Guid ProjectId { get; set; }
         public Guid? UserId { get; set; }
+        [MaxLength(254)]
+        [Required]
+        [EmailAddress]
+        public string EmailAddress { get; set; }
         public Guid InvitationCode { get; set; }
         public bool InvitationSent { get; set; }
         public bool? InvitationAccepted { get; set; }
