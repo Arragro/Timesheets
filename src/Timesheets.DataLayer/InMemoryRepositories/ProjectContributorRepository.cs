bb00a913722b5f555fad34c393b369304ac4974a
Michael Jewell <michael_jewell@hotmail.co.uk>
2014-10-13 18:17:07 +1300

    Added IProjectContributorRepository and In Memory implementation

---
+++ src/Timesheets.DataLayer/InMemoryRepositories/ProjectContributorRepository.cs
@@ -0,0 +1,11 @@
+﻿using Arragro.Common.Repository;
+using System;
+using Timesheets.DataLayer.Interfaces;
+using Timesheets.DataLayer.Models;
+
+namespace Timesheets.DataLayer.InMemoryRepositories
+{
+    public class ProjectContributorRepository : InMemoryRepository<ProjectContributor, Guid>, IProjectContributorRepository
+    {
+    }
+}
