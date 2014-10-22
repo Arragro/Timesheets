6dcfee5bdd9183477e89adbe471e678b9d52244d
Michael Jewell <michael_jewell@hotmail.co.uk>
2014-10-22 18:56:52 +1300

    Refactored

---
+++ src/Timesheets.Tests/Models/ProjectContributorTests.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Timesheets.DataLayer.Models;
+using Xunit;
+
+namespace Timesheets.Tests.Models
+{
+    public class ProjectContributorTests
+    {
+        [Fact]
+        public void ProjectContributor_fails_when_a_null_Project_Invitation_is_supplied()
+        {
+            Assert.Throws<ArgumentNullException>(
+                () =>
+                {
+                    try
+                    {
+                        new ProjectContributor(null);
+                    }
+                    catch (ArgumentNullException ex)
+                    {
+                        Assert.Equal("projectInvitation", ex.ParamName);
+                        throw;
+                    }
+                });
+        }
+
+        [Fact]
+        public void ProjectContributor_fails_when_a_null_or_default_userId_is_supplied()
+        {
+            Assert.Throws<ArgumentNullException>(
+                () =>
+                {
+                    try
+                    {
+                        new ProjectContributor(new ProjectInvitation(new Project("Test", Guid.NewGuid()), string.Empty, new Guid()));
+                    }
+                    catch (ArgumentNullException ex)
+                    {
+                        Assert.Equal("projectInvitation.UserId", ex.ParamName);
+                        throw;
+                    }
+                });
+            Assert.Throws<ArgumentNullException>(
+                () =>
+                {
+                    try
+                    {
+                        new ProjectContributor(new ProjectInvitation(new Project("Test", Guid.NewGuid()), string.Empty, null));
+                    }
+                    catch (ArgumentNullException ex)
+                    {
+                        Assert.Equal("projectInvitation.UserId", ex.ParamName);
+                        throw;
+                    }
+                });
+        }
+    }
+}
---
+++ src/Timesheets.Tests/Models/ProjectInvitationTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Timesheets.DataLayer.Models;
+using Xunit;
+
+namespace Timesheets.Tests.Models
+{
+    public class ProjectInvitationTests
+    {
+        [Fact]
+        public void ProjectInvitation_instantation_fails_with_null_Project()
+        {
+            Assert.Throws<ArgumentNullException>(
+                () =>
+                {
+                    try
+                    {
+                        new ProjectInvitation(null, null);
+                    }
+                    catch (ArgumentNullException ex)
+                    {
+                        Assert.Equal(ex.ParamName, "project");
+                        throw ex;
+                    }
+                });
+        }
+    }
+}
