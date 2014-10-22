2500e06aeefbbc194c011702429425302dccdb4d
Michael Jewell <michael_jewell@hotmail.co.uk>
2014-10-22 21:56:02 +1300

    ProjectContributor missing commit.
    Related Work Items: #150, #151

---
+++ src/Timesheets.BusinessLayer/Services/ProjectContributorService.cs
@@ -0,0 +1,58 @@
+﻿using Arragro.Common.ServiceBase;
+using System;
+using System.Linq;
+using Timesheets.DataLayer.Interfaces;
+using Timesheets.DataLayer.Models;
+
+namespace Timesheets.BusinessLayer.Services
+{
+    public class ProjectContributorService : AuditableService<IProjectContributorRepository, ProjectContributor, Guid, Guid>
+    {
+        public const string DATES_BOTH_MUST_BE_ENTERED_OR_NONE = "A ProjectContributor cannot have either a start or end date, they must have both or none.";
+        public const string END_DATE_MUST_BE_MORE_THAN_OR_EQUAL_TO_START_DATE = "The End Date must be greater or equal to the Start Date.";
+        public const string CONTRIBUTOR_MUST_HAVE_DATES_THAT_ARE_WITHIN_THE_PROJECT_DATES = "The Contributor's Start and End dates must be within the Project Start and End Dates.";
+
+        public ProjectContributorService(
+            IProjectContributorRepository projectContributorRepository)
+            : base(projectContributorRepository)
+        {
+        }
+
+        private void ValidateStartEndDate(ProjectContributor model)
+        {
+            if ((model.StartDate.HasValue && !model.EndDate.HasValue) ||
+                (!model.EndDate.HasValue && model.EndDate.HasValue))
+                RulesException.ErrorForModel(DATES_BOTH_MUST_BE_ENTERED_OR_NONE);
+            else if ((model.StartDate.HasValue && model.EndDate.HasValue) &&
+                model.StartDate.Value.Date >= model.EndDate.Value.Date)
+                RulesException.ErrorForModel(END_DATE_MUST_BE_MORE_THAN_OR_EQUAL_TO_START_DATE);
+        }
+
+        private void ValidAgainstProjectStartEndDates(ProjectContributor model)
+        {
+            var projectStartDate = model.Project.StartDate;
+            var projectEndDate = model.Project.EndDate;
+            if ((projectStartDate.HasValue && projectEndDate.HasValue) &&
+                (model.StartDate.HasValue && model.EndDate.HasValue))
+                if (model.StartDate.Value.Date < projectStartDate.Value.Date ||
+                    model.EndDate.Value.Date > projectEndDate.Value.Date)
+                    RulesException.ErrorForModel(CONTRIBUTOR_MUST_HAVE_DATES_THAT_ARE_WITHIN_THE_PROJECT_DATES);
+        }
+
+        public override ProjectContributor InsertOrUpdate(ProjectContributor model, Guid userId)
+        {
+            var add = default(Guid) == model.ProjectContributorId;
+            if (add) model.SetProjectContributorId();
+            AddOrUpdateAudit(model, userId, add);
+            return Repository.InsertOrUpdate(model, add);
+        }
+
+        protected override void ValidateModelRules(ProjectContributor model)
+        {
+            ValidateStartEndDate(model);
+            ValidAgainstProjectStartEndDates(model);
+
+            if (RulesException.Errors.Any()) throw RulesException;
+        }
+    }
+}
--- src/Timesheets.DataLayer/Models/ProjectContributor.cs
+++ src/Timesheets.DataLayer/Models/ProjectContributor.cs
@@ -11,13 +11,13 @@ namespace Timesheets.DataLayer.Models
         public Guid ProjectId { get; private set; }
         public Guid UserId { get; private set; }
         public ContributorRole ContributorRole { get; private set; }
-        public decimal WeeklyContributorHoursLimit { get; private set; }
+        public decimal? WeeklyContributorHoursLimit { get; private set; }
         public bool RequiresApproval { get; private set; }
-        public DateTime StartDate { get; private set; }
-        public DateTime EndDate { get; private set; }
+        public DateTime? StartDate { get; private set; }
+        public DateTime? EndDate { get; private set; }
         [MaxLength(20)]
         public string PurchaseOrderNumber { get; private set; }
-        public decimal HourlyRate { get; private set; }
+        public decimal? HourlyRate { get; private set; }
 
         public Project Project { get; private set; }
 
@@ -27,9 +27,46 @@ namespace Timesheets.DataLayer.Models
         public ProjectContributor(
             ProjectInvitation projectInvitation)
         {
-            if (projectInvitation == null) throw new ArgumentNullException("projectInviation", "The Project Invitation supplied is null");
-            if (projectInvitation.Project == null) throw new ArgumentNullException("projectInviation", "The Project Invitation supplied is null");
+            if (projectInvitation == null) throw new ArgumentNullException("projectInvitation", "The Project Invitation supplied is null");
+            if (projectInvitation.Project == null) throw new ArgumentNullException("projectInvitation.Project", "The Project supplied is null");
+            if (!projectInvitation.UserId.HasValue ||
+                (projectInvitation.UserId.HasValue && projectInvitation.UserId.Value == default(Guid)))
+                throw new ArgumentNullException("projectInvitation.UserId", "The Project Invitation does not have a UserId");
             ProjectId = projectInvitation.Project.ProjectId;
+            UserId = projectInvitation.UserId.Value;
+            ContributorRole = ContributorRole.User;
+            WeeklyContributorHoursLimit = null;
+            RequiresApproval = false;
+            StartDate = projectInvitation.Project.StartDate;
+            EndDate = projectInvitation.Project.EndDate;
+            PurchaseOrderNumber = null;
+            HourlyRate = null;
+
+            Project = projectInvitation.Project;
+        }
+
+        public void SetProjectContributorId()
+        {
+            if (ProjectContributorId != default(Guid)) throw new Exception("The ProjectContributorId is already set");
+            ProjectContributorId = Guid.NewGuid();
+        }
+
+        public void SetContributorRoles(ContributorRole contributorRole)
+        {
+            ContributorRole = contributorRole;
+        }
+
+        public void SetContributorProjectDetails(
+            decimal? weeklyContributorHoursLimit = null, bool requiresApproval = false,
+            DateTime? startDate = null, DateTime? endDate = null,
+            string purchaseOrderNumber = null, decimal? hourlyRate = null)
+        {
+            WeeklyContributorHoursLimit = weeklyContributorHoursLimit;
+            RequiresApproval = requiresApproval;
+            StartDate = startDate;
+            EndDate = endDate;
+            PurchaseOrderNumber = purchaseOrderNumber;
+            HourlyRate = hourlyRate;
         }
     }
 }
