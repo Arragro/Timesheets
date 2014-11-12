6eccbe9edc4d984d9c32c16883e6460641e03720
Michael Jewell <michael_jewell@hotmail.co.uk>
2014-11-12 21:58:09 +1300

    Changing of the contributor roles
    Related Work Items: #170, #178

---
+++ src/Timesheets.BusinessLayer/Domain/UserProjectContributor.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNet.Identity;
+using System;
+using Timesheets.BusinessLayer.Services;
+using Timesheets.DataLayer.Enums;
+using Timesheets.DataLayer.Models;
+
+namespace Timesheets.BusinessLayer.Domain
+{
+    public class UserProjectContributor
+    {
+        public IUser<Guid> User { get; private set; }
+
+        private readonly SecurityRules _securityRules;
+        private readonly ProjectContributorService _projectContributorService;
+
+        public UserProjectContributor(
+            IUser<Guid> user,
+            SecurityRules securityRules,
+            ProjectContributorService projectContributorService)
+        {
+            if (user == null) throw new ArgumentNullException("user");
+            if (securityRules == null) throw new ArgumentNullException("securityRules");
+            if (projectContributorService == null) throw new ArgumentNullException("projectContributorService");
+
+            User = user;
+            _securityRules = securityRules;
+            _projectContributorService = projectContributorService;
+        }
+
+        private void EnsureUserIdNotOwner(ProjectContributor projectContributor)
+        {
+        }
+
+        public ProjectContributor ChangeProjectContributorsRole(
+            ProjectContributor projectContributor, ContributorRole contributorRole)
+        {
+            _securityRules.IsUserAuthorisedToModifyProjectData(projectContributor.Project, User);
+            projectContributor.SetContributorRole(contributorRole);
+
+            _projectContributorService.ValidateAndInsertOrUpdate(projectContributor, User.Id);
+            _projectContributorService.SaveChanges();
+
+            return projectContributor;
+        }
+    }
+}
